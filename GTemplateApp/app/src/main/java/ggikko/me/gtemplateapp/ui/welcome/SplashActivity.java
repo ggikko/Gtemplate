928e99e2bcf61253be1dfe576903190298c215cc
Jihong Park <ggikko@hotmail.com>
2016-06-25 23:20:30 +0900

    add splash activity, retro lambda, network moduel

---
+++ GTemplateApp/app/src/main/java/ggikko/me/gtemplateapp/ui/welcome/SplashActivity.java
@@ -0,0 +1,21 @@
+package ggikko.me.gtemplateapp.ui.welcome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ggikko.me.gtemplateapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+}
