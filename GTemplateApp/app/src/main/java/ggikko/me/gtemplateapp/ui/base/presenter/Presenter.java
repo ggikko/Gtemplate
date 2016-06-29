5dfd1b319aaf855d32d1122a1d488d4aa37039d4
Jihong Park <ggikko@hotmail.com>
2016-06-29 15:50:50 +0900

    add base presenter

---
+++ GTemplateApp/app/src/main/java/ggikko/me/gtemplateapp/ui/base/presenter/Presenter.java
@@ -0,0 +1,13 @@
+package ggikko.me.gtemplateapp.ui.base.presenter;
+
+/**
+ * Created by admin on 16. 5. 25..
+ */
+public interface Presenter {
+
+    void onCreate();
+    void onPause();
+    void onResume();
+    void onDestroy();
+
+}
