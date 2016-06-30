828e410bad686f267c7643d55655d2fc2084f9f5
Jihong Park <ggikko@hotmail.com>
2016-06-30 15:43:44 +0900

    add drawer listener + offset animation

--- GTemplateApp/app/src/main/java/ggikko/me/gtemplateapp/ui/MainActivity.java
+++ GTemplateApp/app/src/main/java/ggikko/me/gtemplateapp/ui/MainActivity.java
@@ -1,11 +1,19 @@
 package ggikko.me.gtemplateapp.ui;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import ggikko.me.gtemplateapp.R;
@@ -13,6 +21,12 @@ import ggikko.me.gtemplateapp.model.translate.service.TranslateService;
 import ggikko.me.gtemplateapp.ui.base.InjectionActivity;
 
 public class MainActivity extends InjectionActivity {
+
+//    @BindView(R.id.init_navigation_view) NavigationView init_navigation_view;
+//    @BindView(R.id.init_drawer_layout) DrawerLayout init_drawer_layout;
+//    @BindView(R.id.init_toolbar) Toolbar init_toolbar;
+//    private ActionBarDrawerToggle mActionBarDrawerToggle;
+//    private float lastTranslate = 0.0f;
 
     //sample
     @Inject TranslateService mTranslateService;
@@ -35,6 +49,43 @@ public class MainActivity extends InjectionActivity {
         setSupportActionBar(toolbar);
 
 
-
+//        if (init_navigation_view != null) setupDrawerContent(init_navigation_view);
+//        //drawer slide animation
+//        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, init_drawer_layout, init_toolbar, R.string.acc_drawer_open, R.string.acc_drawer_close){
+//            @Override
+//            public void onDrawerSlide(View drawerView, float slideOffset) {
+//                float moveFactor = (invest_list_content.getWidth() * slideOffset)/4;
+//
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+//                {
+//                    invest_list_content.setTranslationX(moveFactor);
+//                }
+//                else
+//                {
+//                    TranslateAnimation anim = new TranslateAnimation(lastTranslate, moveFactor, 0.0f, 0.0f);
+//                    anim.setDuration(0);
+//                    anim.setFillAfter(true);
+//                    invest_list_content.startAnimation(anim);
+//
+//                    lastTranslate = moveFactor;
+//                }
+//            }
+//        };
+//
+//        init_drawer_layout.addDrawerListener(mActionBarDrawerToggle);
     }
+
+    /** drawer item event listener */
+//    private void setupDrawerContent(NavigationView navigationView) {
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(MenuItem menuItem) {
+//                menuItem.setChecked(true);
+//                switch (menuItem.getItemId()) {
+//                }
+//                init_drawer_layout.closeDrawers();
+//                return true;
+//            }
+//        });
+//    }
 }
